82a79d6e1da8a89deee526015b617d1079b55e8c
Kalpesh-Mali <kalpesh.mali@bridgelabz.com>
2019-06-29 11:11:23 +0530

    first commit

---
+++ src/main/java/com/bridgelabs/fundoowalkin/DemoGooge1Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabs.fundoowalkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGooge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGooge1Application.class, args);
+	}
+
+}
